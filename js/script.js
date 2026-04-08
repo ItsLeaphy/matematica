847ebe318ae492de678c38d21d07db003674078b
ItsLeaphy <itsleaphy@gmail.com>
2026-04-08 11:51:58 -0300

    upd

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function toggle(id) {
+  const el = document.getElementById(id);
+
+  if (el.style.display === "none") {
+    el.style.display = "block";
+  } else {
+    el.style.display = "none";
+  }
+}
+
+function toggle(id) {
+  const el = document.getElementById(id);
+
+  if (el.style.display === "none") {
+    el.style.display = "block";
+  } else {
+    el.style.display = "none";
+  }
+}
+
+function esconderTodos(classe) {
+  const elementos = document.querySelectorAll(classe);
+
+  elementos.forEach(el => {
+    el.style.display = "none";
+  });
+}
+
+function esconderTodos(classe) {
+  const elementos = document.querySelectorAll(classe);
+
+  elementos.forEach(el => {
+    el.style.display = "none";
+  });
+}
